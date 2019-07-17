de088cc6391bff94c4ced05dc0a489f04d336c7a
Franky Liu <cuifranky95@gmail.com>
2019-07-17 17:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var output = [];
-  for(counter = 0; counter < musicians.length; counter++){
-    
+  
+  
+  for(var counter = 0; counter < musicians.length; counter++){
+    output[counter] = musicians[counter] + " plays " + instruments[counter];
   }
+  
+  return output;
 }
